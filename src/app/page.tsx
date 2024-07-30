90b6af42d922bb07bf4ebac4c855a046f4497ae0
Michael <mike.michaelmilstead@gmail.com>
2024-07-29 19:54:05 -0700

    more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,23 @@ import { HydraChat } from "hydra-ai-backup";
 
 const Page = () => {
   return (
-    <div className="container mx-auto px-5 mb-10">
+    <div className="container mx-auto px-5 mb-10 flex flex-col justify-center items-center">
       <Header />
-      <HydraChat
-        hydraClient={hydra}
-        initialMessages={[
-          {
-            sender: "Hydra",
-            message: `I am a Hydra-powered AI agent`,
-            type: "text",
-          },
-        ]}
-        inputTextColor="white"
-      />
+      <div className="flex-grow h-full overflow-y-auto  text-xs w-full max-w-2xl rounded-lg ">
+        <HydraChat
+          hydraClient={hydra}
+          initialMessages={[
+            {
+              sender: "Hydra",
+              message: `I am a Hydra-powered AI agent.`,
+              type: "text",
+            },
+          ]}
+          inputTextColor="white"
+          loadingIconColor="gray"
+          aiIconColor="#70bfae"
+        />
+      </div>
     </div>
   );
 };
