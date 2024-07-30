46e9c2cc7c56ca643049af2ddf8597fad6b410c7
Michael <mike.michaelmilstead@gmail.com>
2024-07-29 21:00:15 -0700

    update input color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <div className="container h-[100dvh] mx-auto px-5 pb-10 flex flex-col justify-center items-center">
       <Header />
-      <div className="flex-grow min-h-[400px] overflow-y-auto text-xs w-full max-w-2xl rounded-lg ">
+      <div className="flex-grow min-h-[400px] overflow-y-auto text-xs w-full max-w-2xl rounded-lg bg-secondary">
         <HydraChat
           hydraClient={hydra}
           initialMessages={[
@@ -17,6 +17,7 @@ const Page = () => {
               type: "text",
             },
           ]}
+          inputBackgroundColor="#343437"
           inputTextColor="white"
           loadingIconColor="gray"
           aiIconColor="#70bfae"
