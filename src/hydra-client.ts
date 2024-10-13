0f763e29e91b4d315ca192e389095ff31f3cbf1e
Michael <mike.michaelmilstead@gmail.com>
2024-10-13 09:31:52 -0700

    remove key

--- src/hydra-client.ts
+++ src/hydra-client.ts
@@ -4,7 +4,7 @@ import { SendMessageFormList } from "./components/hydra-components/send-message-
 import { getProfiles } from "./services/profile-service";
 
 const hydra = new HydraClient({
-  hydraApiKey: "41a06403c20a115b6c4a9d93eb4eef0f.cda1a222c93352f270e83a8d3ed3e418ddb87e9bb4a4cc712dd91f3e962943f36ab0d2cfe1bdc0be1649a4e056aede8822a4acd32eb3edfe1671c7fc1ef311b1"
+  hydraApiKey: "<key here>"
 });
 
 const getProfilesTool = {
